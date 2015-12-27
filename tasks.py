3113fc6cba817a47765d2ba499eda46763d7ec4c
Charles Hansen <charlesshansen@gmail.com>
2015-12-27 14:48:40 -0600

    Reorganization of tasks.py, expanded task available on selection of task number

--- tasks.py
+++ tasks.py
@@ -63,6 +63,78 @@ def get_credentials():
 		print('Storing credentials to ' + credential_path)
 	return credentials
 
+def add_task(intcommand, item, service):
+	toadd = { 'title':  intcommand[1] }
+	added = service.tasks().insert(tasklist=item['id'], body=toadd).execute()
+	print('Task {0} added.'.format(added['title']))
+
+def delete_task(rawcommand, alltasks, item, service):
+	todelete = 0
+	fincommand = rawcommand.split(' ')
+	try:
+		for i in range(1, len(fincommand)):
+			for task in alltasks:
+				if(todelete == int(fincommand[i])):
+					deleted = service.tasks().delete(tasklist=item['id'], task=task['id']).execute()
+					print('Task Deleted.')
+				todelete += 1
+			todelete = 0
+	except:
+		pass
+
+
+def complete_task(rawcommand, alltasks, item, service):
+	tocomplete = 0
+	fincommand = rawcommand.split(' ')
+	for i in range(1, len(fincommand)):
+		try:
+			for task in alltasks:
+				if(tocomplete == int(fincommand[i])):
+					toupdate = service.tasks().get(tasklist=item['id'], task=task['id']).execute()
+					toupdate['status'] = 'completed'
+					updated = service.tasks().update(tasklist=item['id'], task=task['id'], body=toupdate).execute()
+					print('Task {0} completed. Good job!'.format(updated['title']))
+				tocomplete += 1
+			tocomplete = 0
+		except:
+			pass
+
+def reopen_task(rawcommand, alltasks, item, service):				
+	toreopen = 0
+	fincommand = rawcommand.split(' ')
+	try:
+		for i in range(1, len(fincommand)):
+			for task in alltasks:
+				if(toreopen == int(fincommand[i])):
+					toupdate = service.tasks().get(tasklist=item['id'], task=task['id']).execute()
+					toupdate['status'] = 'needsAction'
+					toupdate['completed'] = None
+					updated = service.tasks().update(tasklist=item['id'], task=task['id'], body=toupdate).execute()
+					print('Task {0} updated.'.format(updated['title']))
+				toreopen += 1
+			toreopen = 0
+	except:
+		pass
+
+def expanded_task(command, alltasks):
+	disp = 0
+	for disptask in alltasks:
+		if(disp == int(command)):
+			title = disptask['title']
+			status = disptask['status']
+			try:
+				due = disptask['due']
+			except:
+				due = 'No Due Date'
+			try:
+				notes = disptask['notes']
+			except:
+				notes = 'No notes'
+			print('\n{0}:\t{1}\n\tDue: {2}\n\t{3}\n'.format(title, status, due, notes))
+
+		disp += 1
+
+
 def main():
 	"""Shows basic usage of the Google Tasks API.
 
@@ -80,11 +152,30 @@ def main():
 		return
 	else:
 		print('Task lists:')
+		order = 0
 		for item in items:
-			print('{0} ({1})'.format(item['title'], item['id']))
+			print('[{0}] {1}'.format(order, item['title']))
+			order += 1
 		rawlist = raw_in('Which list would you like to use? ')
+		listnum = -1
+		valid = 0
+
+		"""
+		Show available lists and have user select one
+		"""
 		for item in items:
-			if(rawlist.lower() == item['title'].lower()):
+			listnum += 1
+			try:
+				if(int(rawlist) == listnum):
+					valid = 1
+			except:
+				if(rawlist.lower() == item['title'].lower()):
+					valid=1
+
+			"""
+			List commands and prompt user for action
+			"""
+			if(valid):
 				while(1):
 					tasklist = service.tasks().list(tasklist=item['id']).execute()
 					alltasks = tasklist.get('items', [])
@@ -98,62 +189,26 @@ def main():
 		
 
 					if(command.lower() == 'add' or command.lower() == 'a'):
-						toadd = { 'title':  intcommand[1] }
-						added = service.tasks().insert(tasklist=item['id'], body=toadd).execute()
-						print('Task {0} added.'.format(added['title']))
+						add_task(intcommand, item, service)
 
 					elif(command.lower() == 'delete' or command.lower() == 'd'):
-						todelete = 0
-						fincommand = rawcommand.split(' ')
-						try:
-							for i in range(1, len(fincommand)):
-								for task in alltasks:
-									if(todelete == int(fincommand[i])):
-										deleted = service.tasks().delete(tasklist=item['id'], task=task['id']).execute()
-									todelete += 1
-								todelete = 0
-						except:
-							pass
+						delete_task(rawcommand, alltasks, item, service)
 
 					elif(command.lower() == 'complete' or command.lower() == 'c'):
-						tocomplete = 0
-						fincommand = rawcommand.split(' ')
-						for i in range(1, len(fincommand)):
-							try:
-								for task in alltasks:
-									if(tocomplete == int(fincommand[i])):
-										toupdate = service.tasks().get(tasklist=item['id'], task=task['id']).execute()
-										toupdate['status'] = 'completed'
-										updated = service.tasks().update(tasklist=item['id'], task=task['id'], body=toupdate).execute()
-										print('Task {0} completed. Good job!'.format(updated['title']))
-									tocomplete += 1
-								tocomplete = 0
-							except:
-								pass
-
+						complete_task(rawcommand, alltasks, item, service)
+						
 					elif(command.lower() == 'reopen' or command.lower() == 'r'):
-						toreopen = 0
-						fincommand = rawcommand.split(' ')
-						try:
-							for i in range(1, len(fincommand)):
-								for task in alltasks:
-									if(toreopen == int(fincommand[i])):
-										toupdate = service.tasks().get(tasklist=item['id'], task=task['id']).execute()
-										toupdate['status'] = 'needsAction'
-										toupdate['completed'] = None
-										updated = service.tasks().update(tasklist=item['id'], task=task['id'], body=toupdate).execute()
-										print('Task {0} updated.'.format(updated['title']))
-									toreopen += 1
-								toreopen = 0
-						except:
-							pass
+						reopen_task(rawcommand, alltasks, item, service)
 
 					elif(command.lower() == 'quit' or command.lower() == 'q'):
 						return
 
+					elif(command.isdigit()):
+						if(int(command) <= len(alltasks) - 1):
+							expanded_task(command, alltasks)
+
 					else:
 						print("Commands are: add, delete, complete, reopen, quit")
-
 
 if __name__ == '__main__':
 	main()
